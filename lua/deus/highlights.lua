3172eea4048e350e8b89e3233d3ee32159644aaf
genmzy <986299679@qq.com>
2024-08-09 10:32:23 +0800

    feat: bracket

--- lua/deus/highlights.lua
+++ lua/deus/highlights.lua
@@ -497,7 +497,7 @@ highlights.generate_syntax = function(palette, options)
     TSParameterReference = { link = "Fg" },
     TSPreProc = { link = "PreProc" },
     TSProperty = { link = "Blue" },
-    TSPunctBracket = { link = "Fg" },
+    TSPunctBracket = { link = "Grey" },
     TSPunctDelimiter = { link = "Grey" },
     TSPunctSpecial = { link = "Blue" },
     TSRepeat = { link = "Red" },
